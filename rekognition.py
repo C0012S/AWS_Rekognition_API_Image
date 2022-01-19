49d1f90f41f1d7c109a38dc519e039abe4ab8890
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-20 06:18:24 +0900

    Tutorial4 - Facial Analysis _ 여러 명 분석

--- rekognition.py
+++ rekognition.py
@@ -8,7 +8,7 @@ with open('credentials.csv', 'r') as input:
         access_key_id = line[2]
         secret_access_key = line[3]
 
-photo = 'smiling_girl.jpg'
+photo = 'people_faces-1.jpg'
 
 client = boto3.client('rekognition',
                       aws_access_key_id = access_key_id,
@@ -19,7 +19,12 @@ response = client.detect_faces(Image={'S3Object': {
             'Bucket': 'cookies-bucket',
             'Name': photo,
         }},
-                                Attributes = ['ALL'] # AgeRange, Smile 포함시키기 위해 Attributes 설정
+                                Attributes = ['ALL'] # AgeRange, Smile 등을 포함시키기 위해 Attributes 설정
                                                     )
 
-print(response)
+# print(response) # 한 사람의 결과만 출력한다
+for key, value in response.items(): # 전체 사람의 결과를 출력한다
+    if key == 'FaceDetails':
+        for people_att in value:
+            print(people_att)
+            print("======")
