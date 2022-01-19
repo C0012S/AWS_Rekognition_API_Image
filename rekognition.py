72b6afe56de696ee10956db170e7943a148b063a
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-20 04:07:28 +0900

    Tutorial3 - Image Moderation

--- rekognition.py
+++ rekognition.py
@@ -8,18 +8,19 @@ with open('credentials.csv', 'r') as input:
         access_key_id = line[2]
         secret_access_key = line[3]
 
-photo = 'hot_air_balloon.jpg'
+# photo = 'hot_air_balloon.jpg' # ModerationLabels 없다
+photo = 'beyonce.jpg'
 
 client = boto3.client('rekognition',
                       aws_access_key_id = access_key_id,
                       aws_secret_access_key = secret_access_key,
                       region_name="ap-northeast-2") # S3 Bucket의 AWS region과 동일하게 설정
 
-response = client.detect_labels(Image={'S3Object': {
+response = client.detect_moderation_labels(Image={'S3Object': {
             'Bucket': 'cookies-bucket',
             'Name': photo,
-        }},
-                                MaxLabels=2,
-                                MinConfidence=95)
+        }}
+                                #, MinConfidence=9 # MinConfidence가 95보다 작으면 ModerationLabels 없다
+                                                    )
 
 print(response)
